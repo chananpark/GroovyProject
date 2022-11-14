d56c6aac0f076e4645d151d1261d15ef22819e3b
Chanan Park <pca_719@naver.com>
2022-11-14 15:26:52 +0900

    design: side menu 디자인 결정, tiles layout에 side 없는 버전 추가

--- GroovyProject/src/main/java/com/spring/groovy/approval/controller/ApprovalController.java
+++ GroovyProject/src/main/java/com/spring/groovy/approval/controller/ApprovalController.java
@@ -9,27 +9,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ApprovalController {
 
+	// 전자결재 홈 페이지요청
 	@RequestMapping(value = "/approval.on")
+	public String approvalHome(HttpServletRequest request) {
+
+		return "approval/home.tiles";
+	}
+	
+	// 개인문서함-상신함 페이지요청
+	@RequestMapping(value = "/approval/personal/sent.on")
+	public String sentDraftList(HttpServletRequest request) {
+		
+		return "approval/my_draft_sent.tiles";
+	}
+	
+	// 개인문서함-결재함 페이지요청
+	@RequestMapping(value = "/approval/personal/processed.on")
+	public String processdDraftList(HttpServletRequest request) {
+		
+		return "approval/my_draft_processed.tiles";
+	}
+	
+	// 팀문서함 페이지요청
+	@RequestMapping(value = "/approval/team.on")
+	public String teamDraftList(HttpServletRequest request) {
+		
+		return "approval/team_draft.tiles";
+	}
+	
+	// 결재하기-결재대기문서 페이지요청
+	@RequestMapping(value = "/approval/requested.on")
+	public String requestedDraftList(HttpServletRequest request) {
+		
+		return "approval/requested_draft.tiles";
+	}
+	@RequestMapping(value = "/approval/test.on")
 	public String test(HttpServletRequest request) {
-
-		return "approval/home/home.tiles";
-	}
-	
-	@RequestMapping(value = "/approval/personal.on")
-	public String test2(HttpServletRequest request) {
 		
-		return "approval/personal/personalBody.tiles";
+		return "approval/test.tiles";
 	}
-	
-	@RequestMapping(value = "/approval/personal/reception.on")
-	public String test3(HttpServletRequest request) {
-		
-		return "approval/personal/reception/receptionBody.tiles";
-	}
-
-	// 컨트롤러에 테스트 해보겟습니다. 나중에 지워주세용
-	
-	// 두번째 테스트입니다.. 이번에는 에러 없어야만....
-	
-	// 세번째 테스트입니다... 기도하면서 푸쉬합니다...
 }
