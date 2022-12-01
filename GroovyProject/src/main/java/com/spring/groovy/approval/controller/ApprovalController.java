d64aaafeca4da27a55066fddf652d5e5a7913367
Chanan Park <pca_719@naver.com>
2022-12-01 16:16:27 +0900

    fix: Pagination 클래스 수정
    queryString 필드 및 getter, setter를 생성하였으며
    페이지바 생성 메소드에서 url 뒤에 queryString을 더해주도록 수정하였습니다

--- GroovyProject/src/main/java/com/spring/groovy/approval/controller/ApprovalController.java
+++ GroovyProject/src/main/java/com/spring/groovy/approval/controller/ApprovalController.java
@@ -185,7 +185,6 @@ public class ApprovalController {
 		}
 	}
 	
-	
 	// 개인문서함-상신함 페이지요청
 	@SuppressWarnings("unchecked")
 	@RequestMapping(value = "/personal/sent.on")
@@ -207,8 +206,10 @@ public class ApprovalController {
 		// 한 페이지에 표시할 글 목록
 		mav.addObject("draftList", service.getSentDraftList(paraMap));
 
-		// 페이지바
-		mav.addObject("pagebar", pagination.getPagebar(request.getContextPath() + "/approval/personal/sent.on"));
+		// 페이지바 생성
+		String url = request.getContextPath() + "/approval/personal/sent.on";
+		pagination.setQueryString("&sortType="+paraMap.get("sortType")+"&sortOrder="+paraMap.get("sortOrder"));
+		mav.addObject("pagebar", pagination.getPagebar(url));
 		mav.addObject("paraMap", paraMap);
 
 		mav.setViewName("approval/my_draft/sent.tiles");
@@ -238,7 +239,9 @@ public class ApprovalController {
 		mav.addObject("draftList", service.getProcessedDraftList(paraMap));
 
 		// 페이지바
-		mav.addObject("pagebar", pagination.getPagebar(request.getContextPath() + "/approval/personal/processed.on"));
+		String url = request.getContextPath() + "/approval/personal/processed.on";
+		pagination.setQueryString("&sortType="+paraMap.get("sortType")+"&sortOrder="+paraMap.get("sortOrder"));
+		mav.addObject("pagebar", pagination.getPagebar(url));
 		mav.addObject("paraMap", paraMap);
 
 		mav.setViewName("approval/my_draft/processed.tiles");
@@ -268,7 +271,9 @@ public class ApprovalController {
 		mav.addObject("tempDraftList", service.getSavedDraftList(paraMap));
 
 		// 페이지바
-		mav.addObject("pagebar", pagination.getPagebar(request.getContextPath() + "/approval/personal/processed.on"));
+		String url = request.getContextPath() + "/approval/personal/saved.on";
+		pagination.setQueryString("&sortType="+paraMap.get("sortType")+"&sortOrder="+paraMap.get("sortOrder"));
+		mav.addObject("pagebar", pagination.getPagebar(url));
 		mav.addObject("paraMap", paraMap);
 
 		mav.setViewName("approval/my_draft/saved.tiles");
@@ -316,138 +321,13 @@ public class ApprovalController {
 		mav.addObject("draftList", service.getTeamDraftList(paraMap));
 
 		// 페이지바
-		mav.addObject("pagebar", pagination.getPagebar(request.getContextPath() + "/approval/team.on"));
+		String url = request.getContextPath() + "/approval/team.on";
+		pagination.setQueryString("&sortType="+paraMap.get("sortType")+"&sortOrder="+paraMap.get("sortOrder"));
+		mav.addObject("pagebar", pagination.getPagebar(url));
 		mav.addObject("paraMap", paraMap);
 
 		mav.setViewName("approval/team_draft.tiles"); // View
 		return mav;
-	}
-
-	// 문서함 목록 엑셀 다운로드
-	@RequestMapping(value = "excel/downloadExcelFile.on")
-	public String downloadExcelFile(Model model, HttpServletRequest request) {
-
-		Enumeration<String> params = request.getParameterNames();
-
-		HashMap<String, Object> map = new HashMap<String, Object>();
-		while (params.hasMoreElements()) {
-			String key = params.nextElement().toString();
-			String value = request.getParameter(key);
-
-			map.put(key, value);
-		}
-
-		// 다운로드할 글 목록
-		String downloadList = String.valueOf(map.get("downloadList"));
-		String[] downloadArray = downloadList.split(",");
-
-		// 다운로드할 문서함 이름
-		String listName = String.valueOf(map.get("listName"));
-
-		// 헤더 셀에 들어갈 이름 목록
-		String header = String.valueOf(map.get("header"));
-		String[] headerArray = header.split(",");
-		int length = headerArray.length;
-
-		// 워크북 객체 생성
-		SXSSFWorkbook workbook = new SXSSFWorkbook();
-
-		// 시트생성
-		SXSSFSheet sheet = workbook.createSheet(listName);
-
-		// 행의 위치를 나타내는 변수
-		int rowLocation = 0;
-
-		// CellStyle 정렬(Alignment)
-		CellStyle mergeRowStyle = workbook.createCellStyle(); // 셀 병합 스타일
-		mergeRowStyle.setAlignment(HorizontalAlignment.CENTER);
-		mergeRowStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-
-		CellStyle headerStyle = workbook.createCellStyle(); // 헤더스타일
-		headerStyle.setAlignment(HorizontalAlignment.CENTER);
-		headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
-
-		// CellStyle 배경색(ForegroundColor)
-		mergeRowStyle.setFillForegroundColor(IndexedColors.DARK_BLUE.getIndex());
-		mergeRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-		headerStyle.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
-		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-
-		// CellStyle 데이터 포맷 설정하기
-		CellStyle moneyStyle = workbook.createCellStyle();
-		moneyStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("#,##0")); // 천단위 쉼표
-
-		// Cell 폰트(Font) 설정하기
-		Font mergeRowFont = workbook.createFont();
-		mergeRowFont.setFontName("나눔고딕");
-		mergeRowFont.setFontHeight((short) 500);
-		mergeRowFont.setColor(IndexedColors.WHITE.getIndex());
-		mergeRowFont.setBold(true);
-		mergeRowStyle.setFont(mergeRowFont);
-
-		// CellStyle 테두리 Border
-		headerStyle.setBorderTop(BorderStyle.THICK);
-		headerStyle.setBorderBottom(BorderStyle.THICK);
-		headerStyle.setBorderLeft(BorderStyle.THIN);
-		headerStyle.setBorderRight(BorderStyle.THIN);
-
-		// Cell Merge 셀 병합시키기
-
-		// 병합할 행 만들기
-		Row mergeRow = sheet.createRow(rowLocation); // 0번째 행
-
-		// 병합할 행에 셀 스타일 주기
-		for (int i = 0; i < length; i++) {
-			Cell cell = mergeRow.createCell(i);
-			cell.setCellStyle(mergeRowStyle);
-			cell.setCellValue(listName);
-		}
-
-		// 셀 병합하기 => 첫 번째 행을 병합한다.
-		sheet.addMergedRegion(new CellRangeAddress(rowLocation, rowLocation, 0, length - 1)); // 시작 행, 끝 행, 시작 열, 끝 열
-
-		// 헤더 행 생성
-		Row headerRow = sheet.createRow(++rowLocation);
-
-		// 헤더 열 생성
-		for (int i = 0; i < length; i++) {
-			Cell headerCell = headerRow.createCell(i);
-			headerCell.setCellValue(headerArray[i]);
-			headerCell.setCellStyle(headerStyle);
-		}
-
-		// 내용에 해당하는 행 및 셀 생성하기
-		Row bodyRow = null;
-		Cell bodyCell = null;
-
-		// 반복횟수
-		int loop = (int) Math.ceil((downloadArray.length / (length * (1.0))));
-		for (int i = 0, j = 0; i < loop; i++) {
-
-			// 행생성
-			bodyRow = sheet.createRow(i + (rowLocation + 1));
-
-			// 열 생성
-			for (int k = 0; k < length; k++) {
-				bodyCell = bodyRow.createCell(k);
-				bodyCell.setCellValue(downloadArray[j++]);
-			}
-
-		}
-
-		sheet.trackAllColumnsForAutoSizing();
-		// 열 너비 설정
-		for (int i = 0; i < length; i++) {
-			sheet.autoSizeColumn(i);
-			sheet.setColumnWidth(i, (sheet.getColumnWidth(i)) + 700);
-		}
-
-		model.addAttribute("locale", Locale.KOREA);
-		model.addAttribute("workbook", workbook);
-		model.addAttribute("workbookName", listName);
-
-		return "excelDownloadView"; // 엑셀 다운로드 뷰 역할을 해주는 bean
 	}
 
 	// 결재하기-결재대기문서 페이지요청
@@ -483,7 +363,9 @@ public class ApprovalController {
 		mav.addObject("draftList", service.getRequestedDraftList(paraMap));
 
 		// 페이지바
-		mav.addObject("pagebar", pagination.getPagebar(request.getContextPath() + "/approval/requested.on"));
+		String url = request.getContextPath() + "/approval/requested.on";
+		pagination.setQueryString("&sortType="+paraMap.get("sortType")+"&sortOrder="+paraMap.get("sortOrder"));
+		mav.addObject("pagebar", pagination.getPagebar(url));
 		mav.addObject("paraMap", paraMap);
 
 		mav.setViewName("approval/requested_draft.tiles");
@@ -1000,5 +882,131 @@ public class ApprovalController {
 		return "error";
 	}
 
+	// 문서함 목록 엑셀 다운로드
+	@RequestMapping(value = "excel/downloadExcelFile.on")
+	public String downloadExcelFile(Model model, HttpServletRequest request) {
+
+		Enumeration<String> params = request.getParameterNames();
+
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		while (params.hasMoreElements()) {
+			String key = params.nextElement().toString();
+			String value = request.getParameter(key);
+
+			map.put(key, value);
+		}
+
+		// 다운로드할 글 목록
+		String downloadList = String.valueOf(map.get("downloadList"));
+		String[] downloadArray = downloadList.split(",");
+
+		// 다운로드할 문서함 이름
+		String listName = String.valueOf(map.get("listName"));
+
+		// 헤더 셀에 들어갈 이름 목록
+		String header = String.valueOf(map.get("header"));
+		String[] headerArray = header.split(",");
+		int length = headerArray.length;
+
+		// 워크북 객체 생성
+		SXSSFWorkbook workbook = new SXSSFWorkbook();
+
+		// 시트생성
+		SXSSFSheet sheet = workbook.createSheet(listName);
+
+		// 행의 위치를 나타내는 변수
+		int rowLocation = 0;
+
+		// CellStyle 정렬(Alignment)
+		CellStyle mergeRowStyle = workbook.createCellStyle(); // 셀 병합 스타일
+		mergeRowStyle.setAlignment(HorizontalAlignment.CENTER);
+		mergeRowStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+
+		CellStyle headerStyle = workbook.createCellStyle(); // 헤더스타일
+		headerStyle.setAlignment(HorizontalAlignment.CENTER);
+		headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+
+		// CellStyle 배경색(ForegroundColor)
+		mergeRowStyle.setFillForegroundColor(IndexedColors.DARK_BLUE.getIndex());
+		mergeRowStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		headerStyle.setFillForegroundColor(IndexedColors.LIGHT_YELLOW.getIndex());
+		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+		// CellStyle 데이터 포맷 설정하기
+		CellStyle moneyStyle = workbook.createCellStyle();
+		moneyStyle.setDataFormat(HSSFDataFormat.getBuiltinFormat("#,##0")); // 천단위 쉼표
+
+		// Cell 폰트(Font) 설정하기
+		Font mergeRowFont = workbook.createFont();
+		mergeRowFont.setFontName("나눔고딕");
+		mergeRowFont.setFontHeight((short) 500);
+		mergeRowFont.setColor(IndexedColors.WHITE.getIndex());
+		mergeRowFont.setBold(true);
+		mergeRowStyle.setFont(mergeRowFont);
+
+		// CellStyle 테두리 Border
+		headerStyle.setBorderTop(BorderStyle.THICK);
+		headerStyle.setBorderBottom(BorderStyle.THICK);
+		headerStyle.setBorderLeft(BorderStyle.THIN);
+		headerStyle.setBorderRight(BorderStyle.THIN);
+
+		// Cell Merge 셀 병합시키기
+
+		// 병합할 행 만들기
+		Row mergeRow = sheet.createRow(rowLocation); // 0번째 행
+
+		// 병합할 행에 셀 스타일 주기
+		for (int i = 0; i < length; i++) {
+			Cell cell = mergeRow.createCell(i);
+			cell.setCellStyle(mergeRowStyle);
+			cell.setCellValue(listName);
+		}
+
+		// 셀 병합하기 => 첫 번째 행을 병합한다.
+		sheet.addMergedRegion(new CellRangeAddress(rowLocation, rowLocation, 0, length - 1)); // 시작 행, 끝 행, 시작 열, 끝 열
+
+		// 헤더 행 생성
+		Row headerRow = sheet.createRow(++rowLocation);
+
+		// 헤더 열 생성
+		for (int i = 0; i < length; i++) {
+			Cell headerCell = headerRow.createCell(i);
+			headerCell.setCellValue(headerArray[i]);
+			headerCell.setCellStyle(headerStyle);
+		}
+
+		// 내용에 해당하는 행 및 셀 생성하기
+		Row bodyRow = null;
+		Cell bodyCell = null;
+
+		// 반복횟수
+		int loop = (int) Math.ceil((downloadArray.length / (length * (1.0))));
+		for (int i = 0, j = 0; i < loop; i++) {
+
+			// 행생성
+			bodyRow = sheet.createRow(i + (rowLocation + 1));
+
+			// 열 생성
+			for (int k = 0; k < length; k++) {
+				bodyCell = bodyRow.createCell(k);
+				bodyCell.setCellValue(downloadArray[j++]);
+			}
+
+		}
+
+		sheet.trackAllColumnsForAutoSizing();
+		// 열 너비 설정
+		for (int i = 0; i < length; i++) {
+			sheet.autoSizeColumn(i);
+			sheet.setColumnWidth(i, (sheet.getColumnWidth(i)) + 700);
+		}
+
+		model.addAttribute("locale", Locale.KOREA);
+		model.addAttribute("workbook", workbook);
+		model.addAttribute("workbookName", listName);
+
+		return "excelDownloadView"; // 엑셀 다운로드 뷰 역할을 해주는 bean
+	}
 }
 
